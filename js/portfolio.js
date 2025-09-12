6e5789da0142f846fc3b8cba46540f4808ab899b
tahmid888 <tahmidulanon@gmail.com>
2025-09-13 02:19:34 +0600

    Project-section-animated

---
+++ js/portfolio.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll(".portfolio-card");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+cards.forEach((card) => observer.observe(card));
